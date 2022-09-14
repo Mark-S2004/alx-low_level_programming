c9b8c9e6a14dbaf536e1cd2c32b6ba7946234238
Mark Saleh <mark-saleh@outlook.com>
2022-09-14 07:32:22 -0700

    10. add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @n1: First integer
+ * @n2: Second integer
+ * Return: Sum
+ */
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
