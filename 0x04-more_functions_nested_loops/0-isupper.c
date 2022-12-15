73a2bf23663e9ec5576001957b227e4f02719415
Mark Saleh <mark-saleh@outlook.com>
2022-12-15 07:17:14 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Determines whether the character is an uppercase or not
+ *
+ * @c: ASCII code of the character
+ *
+ * Return: 1 if @c is uppercase, 0 otherwise (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
