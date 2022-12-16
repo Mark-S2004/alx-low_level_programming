5269619538edf00ee491b1fa604ffb8989134310
Mark Saleh <mark-saleh@outlook.com>
2022-12-16 15:10:35 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	short num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", num);
+		}
+		if (num != 100)
+		{
+			printf(" ");
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
