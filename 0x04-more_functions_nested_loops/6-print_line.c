6547b8ae549bac33a9e37a52cd5daa4f668699a1
Mark Saleh <mark-saleh@outlook.com>
2022-12-16 14:38:10 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_line - Print a line using the character '_' @n times followed by a new line
+  * @n: The length of the line
+  * Return: None
+  */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n--)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
