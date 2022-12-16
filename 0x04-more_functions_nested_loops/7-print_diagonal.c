b9394473c4811f10fdd3e382fb92c96b23e044fa
Mark Saleh <mark-saleh@outlook.com>
2022-12-16 14:52:11 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Print diagonal line using character '\'
+  * followed by a new line
+  * @n: length of the line
+  * Return: None
+  */
+void print_diagonal(int n)
+{
+	short i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
