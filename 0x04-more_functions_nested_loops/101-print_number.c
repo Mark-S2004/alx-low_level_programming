6e214d695049d9bc9f1165a71e46e1180f8d6df2
Mark Saleh <mark-saleh@outlook.com>
2022-12-16 16:54:44 -0800

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_number - Print an integer using putchar
+  * @n: Number to print
+  * Return: None
+  */
+void print_number(int n)
+{
+	short i = 4, j, temp, start = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	while (i--)
+	{
+		temp = n;
+		for (j = 0; j < i; j++)
+		{
+			temp = temp / 10;
+		}
+		if (temp != 0)
+		{
+			start = 1;
+		}
+		if (start == 1 || i == 0)
+		{
+			_putchar(temp % 10 + '0');
+		}
+	}
+}
