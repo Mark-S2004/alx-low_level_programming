db1d77173be5294772c15b1918dd8c2add5a2aa6
Mark Saleh <mark-saleh@outlook.com>
2022-12-20 07:23:13 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Assign 98 to the variable that @n points to
+  * @n: Pointer that points to the variable that will be updated
+  * Return: None
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
