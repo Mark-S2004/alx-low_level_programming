cef22082b7efb8e58e97ad61ee64a72a634d3e1c
Mark Saleh <mark-saleh@outlook.com>
2022-12-21 09:13:36 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * cap_string - Capitalize all words in a string
+  * @str: Pointer to string
+  * Return: Capitalized string
+  */
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i - 1] == '\n' || str[i - 1] == '\t' ||
+					(str[i - 1] >= ' ' && str[i - 1] <= '/'))
+			{
+				str[i] -= 32;
+			}
+		}
+	}
+
+	return (str);
+}
