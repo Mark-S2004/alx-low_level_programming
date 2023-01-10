dafdb90592183b3ee766bbf9df9785e589cb0777
Mark Saleh <mark-saleh@outlook.com>
2023-01-10 13:38:16 -0800

    Make 0-memset.c betty compliant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-  * _memset - fills the first @n bytes of the memory area pointed to by @s with the constant byte @b
+  * _memset - fills the first @n bytes of the memory area pointed to by @s
+  * with the constant byte @b
   * @s: Pointer to character
   * @b: Character to fill the memory
   * @n: Number of bytes to be filled
