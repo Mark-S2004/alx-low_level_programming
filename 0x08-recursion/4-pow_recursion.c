7402e01866309eaee4f1d5b42d09981f5d84d400
Mark Saleh <mark-saleh@outlook.com>
2023-01-18 15:40:01 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - Calculate @x to the power of @y
+  * @x: integer
+  * @y: integer
+  * Return: @x to the power of @y
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, --y));
+}
