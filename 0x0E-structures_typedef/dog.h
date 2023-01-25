7ddd57816a43e2916b6abc1e8bbff8906c24e899
Mark Saleh <mark-saleh@outlook.com>
2023-01-25 05:26:08 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - Dog attributes
+  * @name: Dog name
+  * @age: Dog age
+  * @owner: Dog owner
+  *
+  * Description: This Structure has attributes of a dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
