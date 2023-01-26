654b5db310c7a05fd12e3891f978f5f576901e93
Mark Saleh <mark-saleh@outlook.com>
2023-01-26 06:57:29 -0800

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - Pass each element in @array to @action
+  * @array: Array of integers
+  * @size: Size of @arr
+  * @action: Function returns none and takes an integer as a parameter
+  * Return: None
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!size || !action)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
