0d27869b839edf24ec6d61f666c90ca63f259f03
Mark Saleh <mark-saleh@outlook.com>
2023-01-26 04:54:02 -0800

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Print the name of the file it was compiled from, followed by \n
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf(__FILE__"\n");
+
+	return (0);
+}
