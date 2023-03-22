21bc31127c59a8f21b256263a5b304a2a8a1baaf
Mark Saleh <mark-saleh@outlook.com>
2023-03-22 07:40:04 -0700

    Comply 7-insert_dnodeint.c file to betty style Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-
 	if (!idx)
 	{
 		if (*h)
@@ -50,6 +49,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next->prev = new_node;
 	}
-
 	return (new_node);
 }
